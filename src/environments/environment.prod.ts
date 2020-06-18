23c624a347016e1ae6cfbafa90eb1d2b457a768c
Victor Tsay <vt0826@hotmail.com>
2020-06-18 14:11:26 -0400

    config setting

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBJD4TmdTU_MfsDiv21_-ndrm7gGYIGfsI',
+    authDomain: 'recipe-a78ee.firebaseapp.com',
+    databaseURL: 'https://recipe-a78ee.firebaseio.com',
+    projectId: 'recipe-a78ee',
+    storageBucket: 'recipe-a78ee.appspot.com',
+    messagingSenderId: '853768822956',
+    appId: '1:853768822956:web:2021530687e6e437dfbf09',
+    measurementId: 'G-J1H5FRGFFZ',
+  },
 };
