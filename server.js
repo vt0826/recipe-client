1bab0f28a60ba20d0571f543f13b49374ed16eba
Victor Tsay <vt0826@hotmail.com>
2020-06-18 18:41:14 -0400

    first

--- server.js
+++ server.js
@@ -5,10 +5,19 @@ const path = require("path");
 const app = express();
 
 // Serve only the static files form the angularapp directory
-app.use(express.static(__dirname + "/dist"));
+/* app.use(express.static(__dirname + "/dist")); */
+app.use(express.static(__dirname, "dist", { index: false }));
 
-app.get("*", function (req, res) {
+/* app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname + "/dist/recipe/index.html"));
+}); */
+
+app.get("", function (req, res) {
+  res.sendFile(path.join(__dirname, "src", "index.html"));
+});
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "src", "index.html"));
 });
 
 // Start the app by listening on the default Heroku port
